ca80c7c35347611842ed61b9576a2879c2ec276f
oussama <oussamamikbal123@gmail.com>
2025-07-20 18:54:12 +0100

    fix: correct spelling of 'upload'

---
+++ alx-react-app-new/src/components/Footer.jsx
@@ -0,0 +1,8 @@
+function Footer() {
+    return(
+            <footer>
+                <p style={{backgroundColor: 'blue', color: 'black', borderRadius: '30px'}}>© 2023 City Lovers</p>
+            </footer>
+    );
+}
+export default Footer;
---
+++ alx-react-app-new/src/components/MainContent.jsx
@@ -0,0 +1,8 @@
+function MainContent() {
+    return (
+            <main>
+                <p style={{backgroundColor: '#777', color: 'black', borderRadius: '30px'}}>I love to visit New York, Paris, and Tokyo.</p>
+            </main>
+    );
+}
+export default MainContent;
