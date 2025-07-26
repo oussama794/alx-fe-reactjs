8b702b9190f90a0e8986e6aa4a5c374b738fb7db
oussama <oussamamikbal123@gmail.com>
2025-07-26 18:58:13 +0100

    Re-upload the alx-react-app-props folder

--- alx-react-app-props/src/App.jsx
+++ alx-react-app-props/src/App.jsx
@@ -1,10 +1,10 @@
 import React from 'react';
 import UserDetails from './components/UserDetails';
 import UserInfo from './components/UserInfo';
-import UserContext from './UserContext';
+import UserContext from '/UserContext';
 import ProfilePage from './components/ProfilePage';
 import WelcomeMessage from './components/WelcomeMessage';
-import UserProfile  from './components/UserProfile';
+import UserProfile from './components/UserProfile';
 import Header from './components/Header';
 import MainContent from './components/MainContent';
 import Footer from './components/Footer';
@@ -20,26 +20,6 @@ function App() {
   return (
     <>
       <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-      <div>
         <WelcomeMessage />
       </div>
       <div>
@@ -47,15 +27,15 @@ function App() {
         <MainContent />
         <Footer />
       </div>
-       <UserProfile
+      <UserProfile
         name="Alice"
         age="25"
         bio="Loves hiking and photography"
       />
       <UserContext.Provider value={userData}>
         <ProfilePage />
-        <UserDetails />
         <UserInfo />
+        <UserDetails />
       </UserContext.Provider>
     </>
   )
--- alx-react-app-props/src/components/UserDetails.jsx
+++ alx-react-app-props/src/components/UserDetails.jsx
@@ -1,7 +1,7 @@
-import {useContext} from 'react';
-import UserContext from '../UserContext';
+import { useContext } from 'react';
+import UserContext from '../../UserContext';
 function UserDetails() {
-    const userData = useContext(UserContext);
+  const userData = useContext(UserContext);
   return (
     <div>
       <p>Name: {userData.name}</p>
