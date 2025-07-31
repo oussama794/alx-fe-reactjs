7e9a996afa04e8f50d4a580aca1ffad17d6c372f
oussama <oussamamikbal123@gmail.com>
2025-07-31 13:57:10 +0100

    fixing the Router  in App.jsx

--- recipe-sharing-app/src/App.jsx
+++ recipe-sharing-app/src/App.jsx
@@ -1,22 +1,28 @@
-import { Routes, Route } from 'react-router-dom';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import AddRecipeForm from './components/AddRecipeForm';
 import RecipeList from './components/RecipeList';
 import RecipeDetails from './components/RecipeDetails';
 
 function App() {
   return (
-    <Routes>
-      <Route path="/" element={
-        <>
-          <h1>Recipe Sharing App</h1>
-          <AddRecipeForm />
-          <RecipeList />
-        </>
-      } />
-      <Route path="/recipes/:id" element={<RecipeDetails />} />
-    </Routes>
+    <BrowserRouter>
+      <Routes>
+        <Route
+          path="/"
+          element={
+            <>
+              <h1>Recipe Sharing App</h1>
+              <AddRecipeForm />
+              <RecipeList />
+            </>
+          }
+        />
+        <Route path="/recipes/:id" element={<RecipeDetails />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
 export default App;
+
 
